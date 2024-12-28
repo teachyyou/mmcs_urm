12232e16444046daf3632a9a95bd225c53540c99
teachyyou <kudriavtsiev_2004@mail.ru>
2024-12-28 14:03:03 +0300

    fix machine controller tests

--- spec/controller/machine_spec.rb
+++ spec/controller/machine_spec.rb
@@ -1,114 +1,116 @@
 require "rails_helper"
 
 RSpec.describe MachinesController, type: :controller do
-  let(:user) { create(:user) }
-  let(:other_user) { create(:user) }
-  let(:valid_attributes) do
-    {
-      name: "Test Machine",
-      description: "A test machine",
-      author: user.id,
-      input_counts: 2,
-      instructions: ["1. x1 = 1", "2. stop"]
-    }
+  let(:user) do
+    User.create(
+      name: "Test User",
+      email: "test_user@example.com",
+      password: "password",
+      password_confirmation: "password"
+    )
   end
 
-  let(:invalid_attributes) do
-    {
-      name: "",
-      description: "",
-      input_counts: -1,
-      instructions: []
-    }
+  let(:machine) do
+    Machine.create(
+      name: "Test Machine",
+      description: "A machine for testing",
+      author: user.id,
+      input_counts: 2,
+      instructions: [
+        "1. x1 = 1",
+        "2. if x1 == 0 goto 4 else goto 3",
+        "3. x1 = x1 + 1",
+        "4. stop"
+      ]
+    )
   end
 
   before do
-    sign_in user
+    allow(controller).to receive(:current_user).and_return(user)
   end
 
   describe "GET #index" do
     it "returns a success response with machines" do
-      machine = Machine.create!(valid_attributes)
       get :index, format: :json
-      expect(response).to have_http_status(:ok)
-      expect(JSON.parse(response.body)["machines"].first["name"]).to eq(machine.name)
+      expect(response).to have_http_status(:success)
+      expect(JSON.parse(response.body)["machines"].length).to eq(Machine.count)
     end
   end
 
   describe "GET #show_machine" do
     it "returns the machine details" do
-      machine = Machine.create!(valid_attributes)
-      get :show_machine, params: { id: machine.id }
-      expect(response).to have_http_status(:ok)
-      parsed_response = JSON.parse(response.body)
-      expect(parsed_response["name"]).to eq(machine.name)
-      expect(parsed_response["description"]).to eq(machine.description)
+      get :show_machine, params: { id: machine.id }, format: :json
+      expect(response).to have_http_status(:success)
+      json = JSON.parse(response.body)
+      expect(json["name"]).to eq(machine.name)
+      expect(json["description"]).to eq(machine.description)
     end
   end
 
   describe "POST #create" do
-    context "with valid parameters" do
-      it "creates a new Machine" do
-        expect do
-          post :create, body: valid_attributes.to_json, as: :json
-        end.to change(Machine, :count).by(1)
-        expect(response).to have_http_status(:created)
-      end
+    it "creates a new Machine with valid parameters" do
+      post :create, body: {
+        name: "New Machine",
+        description: "Another test machine",
+        author: user.id,
+        input_counts: 3,
+        instructions: ["1. x1 = 2", "2. stop"]
+      }.to_json, as: :json
+      expect(response).to have_http_status(:created)
+      json = JSON.parse(response.body)
+      expect(json["name"]).to eq("New Machine")
     end
 
-    context "with invalid parameters" do
-      it "returns unprocessable entity" do
-        post :create, body: invalid_attributes.to_json, as: :json
-        expect(response).to have_http_status(:unprocessable_entity)
-        expect(JSON.parse(response.body)["errors"]).to be_present
-      end
+    it "returns unprocessable entity with invalid parameters" do
+      post :create, body: { name: "" }.to_json, as: :json
+      expect(response).to have_http_status(:unprocessable_entity)
+      expect(JSON.parse(response.body)["errors"]).not_to be_empty
     end
   end
 
   describe "PUT #update" do
-    let(:machine) { Machine.create!(valid_attributes) }
-
-    context "when the user is the author" do
-      it "updates the machine and returns success" do
-        updated_data = { name: "Updated Machine" }
-        put :update, params: { id: machine.id }, body: { machine: updated_data }.to_json, as: :json
-        expect(response).to have_http_status(:ok)
-        expect(machine.reload.name).to eq("Updated Machine")
-      end
+    it "updates the machine when the user is the author" do
+      put :update, params: { id: machine.id }, body: {
+        machine: { name: "Updated Machine" }
+      }.to_json, as: :json
+      expect(response).to have_http_status(:ok)
+      json = JSON.parse(response.body)
+      expect(json["name"]).to eq("Updated Machine")
     end
 
-    context "when the user is not the author" do
-      before { sign_in other_user }
-
-      it "returns forbidden" do
-        updated_data = { name: "Updated Machine" }
-        put :update, params: { id: machine.id }, body: { machine: updated_data }.to_json, as: :json
-        expect(response).to have_http_status(:forbidden)
-      end
+    it "returns forbidden when the user is not the author" do
+      another_user = User.create(
+        name: "Another User",
+        email: "another@example.com",
+        password: "password",
+        password_confirmation: "password"
+      )
+      allow(controller).to receive(:current_user).and_return(another_user)
+      put :update, params: { id: machine.id }, body: {
+        machine: { name: "Should Not Update" }
+      }.to_json, as: :json
+      expect(response).to have_http_status(:forbidden)
     end
   end
 
   describe "DELETE #destroy" do
-    let!(:machine) { Machine.create!(valid_attributes) }
-
-    context "when the user is the author" do
-      it "destroys the machine and redirects to index" do
-        expect do
-          delete :destroy, params: { id: machine.id }
-        end.to change(Machine, :count).by(-1)
-        expect(response).to redirect_to(machines_path)
-      end
+    it "destroys the machine when the user is the author" do
+      delete :destroy, params: { id: machine.id }
+      expect(response).to redirect_to(machines_path)
+      expect(Machine.exists?(machine.id)).to be_falsey
     end
 
-    context "when the user is not the author" do
-      before { sign_in other_user }
-
-      it "does not destroy the machine and redirects to index" do
-        expect do
-          delete :destroy, params: { id: machine.id }
-        end.not_to change(Machine, :count)
-        expect(response).to redirect_to(machines_path)
-      end
+    it "does not destroy the machine when the user is not the author" do
+      another_user = User.create(
+        name: "Another User",
+        email: "another@example.com",
+        password: "password",
+        password_confirmation: "password"
+      )
+      allow(controller).to receive(:current_user).and_return(another_user)
+      delete :destroy, params: { id: machine.id }
+      expect(response).to redirect_to(machines_path)
+      expect(Machine.exists?(machine.id)).to be_truthy
     end
   end
 end
