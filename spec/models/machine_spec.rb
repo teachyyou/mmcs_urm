2a131686e3878fc7356d1a5c519be688948e16d5
YBarsukova <yaroslavabarsukova@internet.ru>
2024-12-28 13:06:56 +0300

    model tests

--- spec/models/machine_spec.rb
+++ spec/models/machine_spec.rb
@@ -1,17 +1,46 @@
-# frozen_string_literal: true
-
 require 'rails_helper'
+require "urm/machine"
 
 RSpec.describe Machine, type: :model do
-  describe "validations" do
-    it "validates presence of name" do
-      machine = Machine.new(name: nil)
+  describe "validations and operations" do
+    let(:machine_data) do
+      {
+        name: "Test Machine",
+        description: "A machine for testing",
+        input_counts: "2",
+        instructions: [
+          "1. x1 = 1",
+          "2. if x1 == 0 goto 4 else goto 3",
+          "3. x1 = x1 + 1",
+          "4. stop"
+        ],
+        author: "test_author"
+      }
+    end
 
-      # Замокаем метод valid?, чтобы он всегда возвращал true
-      allow(machine).to receive(:valid?).and_return(true)
+    let(:machine_object) do
+      Urm::Machine.new(machine_data[:input_counts].to_i).tap do |machine|
+        machine.add_all(machine_data[:instructions].map(&:to_s))
+      end
+    end
 
-      expect(machine.valid?).to be_truthy
+    it "creates a Machine object from view data" do
+
+      expect { machine_object }.not_to raise_error
+      expect(machine_object.instructions.compact.size).to eq(machine_data[:instructions].size)
+    end
+
+    it "executes instructions from the machine's data" do
+
+      result = machine_object.run(0, 0)
+      expect(result).to eq(1)
+    end
+
+    it "validates machine's instructions separately" do
+      machine_data[:instructions].each do |instruction|
+        urm_instruction = Urm::Instruction.parse(instruction)
+        expect(urm_instruction).to be_a(Urm::Instruction)
+      end
     end
   end
 end
-
